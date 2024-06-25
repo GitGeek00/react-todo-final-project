b5ba5aede2043c641fdc3fdc5d27a5cd3a6bacf3
Maher Algepah <maher@mahersoft.onmicrosoft.com>
2024-06-25 11:20:16 -0700

    just landing

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+const app = () => {
+
+  return (
+    <>
+      <div id="mainPageDiv">
+        <div id="mainDivContainer">
+          <div id="mainPageTitle">TODO</div>
+          <div id="mainPageSubTitle">FUSION</div>
+          <div id="mainPagePhrase">Handle your <b><i>biz</i></b><div>Like a boss</div></div>
+          <button id="mainPageBtn">Get Started</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default app
